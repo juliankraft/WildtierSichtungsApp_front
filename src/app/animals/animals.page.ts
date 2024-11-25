436b2df8c4388346713b02640c43b2f2876f9a83
juliankraft <kraftjulian@gmail.com>
2024-11-25 17:43:50 +0100

    drag a marker!

--- src/app/animals/animals.page.ts
+++ src/app/animals/animals.page.ts
@@ -11,36 +11,15 @@ import { Geolocation } from '@capacitor/geolocation';
 })
 export class AnimalsPage implements OnInit {
   animals: any;
-  listeSichtbar = false;
-  showMap = false;
-
+  step = 0;
+  googlePositionSet = false;
+  dataset: any = {};
   geolocation = Geolocation;
+  googleoptions: google.maps.MapOptions = {};
+  googlePosition: google.maps.LatLngLiteral = {lat: 0, lng: 0};
+  newPosition: google.maps.LatLngLiteral = {lat: 0, lng: 0};
 
 
-  googleoptions: google.maps.MapOptions = {
-    center : {lat:47.724011 , lng: 8.660298 },
-    zoom: 10
-  };
-
-  constructor(private api:ApiService) {
-  }
-
-
-
-  async setCurrentPosition() {
-    let coordinates = await Geolocation.getCurrentPosition();
-    console.log('Current position:', coordinates);
-    Geolocation.getCurrentPosition().then((position) => {
-      console.log('Current position:', position);
-      this.googleoptions.center = {lat: position.coords.latitude, lng: position.coords.longitude};
-      this.googleoptions.zoom = 10;
-      this.showMap = true;
-    });
-  };
-
-  toggleListeSichtbar() {
-    this.listeSichtbar = !this.listeSichtbar;
-  }
 
   ngOnInit() {
 
@@ -49,6 +28,59 @@ export class AnimalsPage implements OnInit {
       //store the response in the animals variable
       this.animals = data
       console.log(this.animals)
+      this.step = 1;
+      this.setCurrentPositionAndStep();
+    });
+
+  }
+
+  constructor(private api:ApiService) {
+  }
+
+  setAnimal(animal:any){
+    //this.animal = animal;
+    this.dataset.animal = animal;
+  }
+
+  async setCurrentPositionAndStep() {
+    let coordinates = await Geolocation.getCurrentPosition();
+    console.log('Current position:', coordinates);
+    Geolocation.getCurrentPosition().then((position) => {
+      console.log('Current position:', position);
+      this.googleoptions.center = {lat: position.coords.latitude, lng: position.coords.longitude};
+      this.googleoptions.zoom = 15;
+      this.googleoptions.mapId = 'd8f1c2e085cdc36';
+      this.googlePosition = {lat: position.coords.latitude, lng: position.coords.longitude};
+      this.step = 3;
+      this.googlePositionSet=true;
+
+    });
+  };
+
+
+  // Handle marker drag end
+  onMarkerPositionChange(event: any) {
+    if (event && event.latLng) {
+      console.log('Marker position updated:', event.latLng.lat(), event.latLng.lng());
+      this.googlePosition = {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+      };
+    }
+  }
+
+  // Save the current marker position
+  savePosition() {
+    console.log(this.googlePosition);
+    //read map-marker position
+
+  }
+
+  save(){
+    alert('saved');
+    this.api.post('animals', this.dataset).subscribe((data:any)=>{
+
     });
   }
+
 }
