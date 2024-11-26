f8ef4b4949ff5833cb913186e7132780691b3e69
juliankraft <kraftjulian@gmail.com>
2024-11-26 18:21:50 +0100

    getting items to store properly and list design improved

--- src/app/animals/animals.page.ts
+++ src/app/animals/animals.page.ts
@@ -22,7 +22,6 @@ export class AnimalsPage implements OnInit {
 
 
   ngOnInit() {
-
     //get request to the api
     this.api.get('animals').subscribe((data:any)=>{
       //store the response in the animals variable
@@ -31,32 +30,20 @@ export class AnimalsPage implements OnInit {
       this.step = 1;
       // this.setCurrentPositionAndStep();
     });
-
   }
 
   constructor(private api:ApiService) {
   }
 
-  setAnimal(animal:any){
+  setAnimal(animal: any){
     //this.animal = animal;
-    this.dataset.animal = animal;
+    this.dataset.animal_id =  Number(animal.detail.value);
   }
 
-  async setCurrentPositionAndStep() {
-    let coordinates = await Geolocation.getCurrentPosition();
-    console.log('Current position:', coordinates);
-    Geolocation.getCurrentPosition().then((position) => {
-      console.log('Current position:', position);
-      this.googleoptions.center = {lat: position.coords.latitude, lng: position.coords.longitude};
-      this.googleoptions.zoom = 15;
-      this.googleoptions.mapId = 'd8f1c2e085cdc36';
-      this.googlePosition = {lat: position.coords.latitude, lng: position.coords.longitude};
-      this.step = 3;
-      this.googlePositionSet=true;
-
-    });
-  };
-
+  getAnimalNameById(id: number): string {
+    const animal = this.animals.find((animal: { id: number; name: string }) => animal.id === id);
+    return animal ? animal.name : '';
+  }
 
   // Handle marker drag end
   onMarkerPositionChange(event: any) {
@@ -69,17 +56,54 @@ export class AnimalsPage implements OnInit {
     }
   }
 
-  // Save the current marker position
-  savePosition() {
-    console.log(this.googlePosition);
-    //read map-marker position
-
+  formatDateForDisplay(date: string): string {
+    return date.replace('T', ' ');
   }
 
-  save(){
-    alert('saved');
-    this.api.post('animals', this.dataset).subscribe((data:any)=>{
 
+  // ending step 1 - inquerie of animal
+  setCurrentDataAndStep(){
+    if (this.dataset.animal_id == null){
+      alert('Bitte wähle eine Tierart aus');
+      return
+    }
+    if (this.dataset.count_female == null && this.dataset.count_male == null && this.dataset.count_unknown == null) {
+      alert('mindestens eine Anzahl muss angegeben werden');
+      return
+    }
+
+    this.dataset.date = new Date().toISOString().slice(0, -5);
+    this.step = 2
+  }
+
+  // ending step 2 - inquerie of Date
+  async setCurrentPositionAndStep() {
+    let coordinates = await Geolocation.getCurrentPosition();
+    console.log('Current position:', coordinates);
+    Geolocation.getCurrentPosition().then((position) => {
+      console.log('Current position:', position);
+      this.googleoptions.center = {lat: position.coords.latitude, lng: position.coords.longitude};
+      this.googleoptions.zoom = 15;
+      this.googleoptions.mapId = 'd8f1c2e085cdc36';
+      this.googlePosition = {lat: position.coords.latitude, lng: position.coords.longitude};
+      this.googlePositionSet=true;
+      this.step = 3;
+    });
+  };
+
+  // ending step 3 - inquerie of location
+  savePosition() {
+    console.log(this.googlePosition);
+    this.dataset.latitude = this.googlePosition.lat;
+    this.dataset.longitude = this.googlePosition.lng;
+    this.step = 4;
+  }
+
+  // ending step 4 - save the current dataset
+  saveData(){
+    alert('Sichtung erfolgreich gespeichert');
+    this.api.post('animals', this.dataset).subscribe((data:any)=>{
+    this.step = 1;
     });
   }
 
